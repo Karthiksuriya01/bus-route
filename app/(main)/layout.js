b61b816cb43e82b08cf900dbc8ffa012c2023f62
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-03-31 02:22:18 +0530

    updated

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -4,8 +4,10 @@ import Header from "@/components/header";
 export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header/>
-      <main className="flex-1">
+      <div className="">
+        <Header/>
+      </div>
+      <main className="flex-1 mt-15">
         {children}
       </main>
       
