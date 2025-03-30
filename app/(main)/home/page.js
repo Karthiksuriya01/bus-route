b25a30dc265427f41b4b5489993c96f6b041301f
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-03-31 00:12:53 +0530

    updated

--- app/(main)/home/page.js
+++ app/(main)/home/page.js
@@ -1,12 +1,11 @@
 import BusCard from '@/components/bus-card';
-import Header from '@/components/header';
 import Service from '@/components/services';
 import React from 'react';
 
 const  Home= () => {
   return (
-    <div className='m-5'>
-      <Header/>
+    <div >
+     
       <Service/>
       <BusCard/>
     </div>
--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,13 @@
 import { BottomNav } from "@/components/bottomNav";
+import Header from "@/components/header";
 
 export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <main className="flex-1">
+      <div className="m-5">
+        <Header/>
+      </div>
+      <main className="flex-1 m-5">
         {children}
       </main>
       <BottomNav />
