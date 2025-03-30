87ae3094fe93dee3b5ccdb414abbb7014fc0e672
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-03-31 02:15:35 +0530

    updated

--- components/header.js
+++ components/header.js
@@ -5,15 +5,16 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="flex items-center p-4 bg-white shadow-sm">
-      <div className="flex items-center">
+      <div className="flex items-center rounded-sm border-none shadow-2xl bg-amber-700 overflow-hidden">
         <Image 
           src="/logo.png"
           alt="Bus Tracking Logo"
-          width={40}
-          height={40}
+          width={35}
+          height={35}
           className="object-contain"
-        /><h1 className='text-3xl font-semibold mx-0.5'>BusMate</h1>
+        />
       </div>
+        <h1 className='text-3xl font-semibold mx-0.5'>BusMate</h1>
     </header>
   );
 }
