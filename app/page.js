728c1fde63b5b2590af8ef2daadc0312f86b4815
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-03-30 22:40:43 +0530

    upded

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-?import React from 'react';
-import Login from './(auth)/login/page';
+import React from 'react';
+
+import Home from './(main)/home/page';
 
 const Page = () => {
   return (
     <div>
-      
+      <Home/>
     </div>
   );
 }
