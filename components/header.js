ebb579671ec2b858fa8dd836e4b6139485faf3d2
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-03-31 02:11:39 +0530

    updated

--- components/header.js
+++ components/header.js
@@ -1,11 +1,19 @@
 "use client"
+import Image from 'next/image';
 import React from 'react';
-
 
 const Header = () => {
   return (
-    <header className="flex justify-center flex-row items-center shadow-2xl h-12 p-3 borderbg radius-2xl">
-      <h1>Header</h1>
+    <header className="flex items-center p-4 bg-white shadow-sm">
+      <div className="flex items-center">
+        <Image 
+          src="/logo.png"
+          alt="Bus Tracking Logo"
+          width={40}
+          height={40}
+          className="object-contain"
+        /><h1 className='text-3xl font-semibold mx-0.5'>BusMate</h1>
+      </div>
     </header>
   );
 }
